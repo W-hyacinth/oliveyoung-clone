3ae09044369efe9d07e62c66709f1aa49c0116a9
MostOneBrush <w_hyacinth.choi@kakao.com>
2021-07-14 17:00:43 +0900

    앱 다운로드 배너 내 아이콘 사이즈 조절

---
+++ src/components/MainBanner.vue
@@ -0,0 +1,21 @@
+<template>
+  <swiper ref="mainBannerSwiper" :options="swiperOption">
+    <swiper-slide>
+    </swiper-slide>
+  </swiper>
+</template>
+
+<script>
+import {swiper, swiperSlide} from 'vue-awesome-swiper'
+export default {
+  name: 'MainBanner',
+  components: {
+    swiper,
+    swiperSlide
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
