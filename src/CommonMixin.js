7c8f014224aba263f58a5c3830a46f19aeee6fb5
MostOneBrush <w_hyacinth.choi@kakao.com>
2021-07-14 15:27:46 +0900

    헤더 배너와 관련 작업 추가 (쿠키 관련 mixin 함수 추가)

--- src/CommonMixin.js
+++ src/CommonMixin.js
@@ -8,7 +8,7 @@ Vue.mixin({
     }
   },
   methods: {
-    checkMobile: function () {
+    checkMobileResolution: function () {
       this.isMobile = (window.innerWidth < 1020)
     },
     commingSoon: function () {
@@ -17,16 +17,42 @@ Vue.mixin({
     goBack: function () {
       this.$router.go(-1)
     },
+    setCookie: function (name, value, period) {
+      let date = new Date()
+      date.setDate(date.getDate() + period)
+      document.cookie = `${name}=${value};Expires=${date.toUTCString()}`
+    },
+    getCookie: function (name) {
+      const value = document.cookie.match(`(^|;) ?${name}=([^;]*)(;|$)`)
+      return value ? value[2] : null
+    },
+    delCookie: function (name) {
+      let date = new Date()
+      date.setDate(date.getDate() - 100)
+      document.cookie = `${name}=;Expires=${date.toUTCString()}`
+    },
     handleResize: function () {
       this.$nextTick(() => {
-        this.checkMobile()
+        this.checkMobileResolution()
       })
+    }
+  },
+  computed: {
+    mobileDevice: function () {
+      const ua = navigator.userAgent.toLowerCase()
+      if (ua.indexOf('android') > -1) {
+        return 'android'
+      } else if (ua.indexOf('iphone') > -1 || ua.indexOf('ipad') > -1 || ua.indexOf('ipod') > -1) {
+        return 'ios'
+      } else {
+        return 'other'
+      }
     }
   },
   watch: {
   },
   created () {
-    this.checkMobile()
+    this.checkMobileResolution()
   },
   mounted () {
     window.addEventListener('resize', this.handleResize)
---
+++ src/components/HeaderBanner.vue
@@ -0,0 +1,80 @@
+<template>
+  <article class="header-banner">
+    <a :href="oliveAppLink" class="header-banner__link" target="_blank" rel="noopener noreferrer">
+      <span class="header-banner__msg">APP 첫구매 <strong class="header-banner__msg--highlight">5천원 할인 쿠폰과 추가혜택! </strong>받기</span>
+    </a>
+    <button type="button" class="header-banner__btn" aria-label="배너 오늘 하루 닫기" @click="closeBanner">
+      <IconClose color="#fff" />
+    </button>
+  </article>
+</template>
+
+<script>
+import IconClose from '@/components/IconClose'
+export default {
+  name: 'HeaderBanner',
+  methods: {
+    closeBanner: function () {
+      const checkCloseBanner = confirm('오늘 하루 배너를 보지 않겠습니다.')
+      if (checkCloseBanner) {
+        this.setCookie('headerBanner', false, 1)
+        this.$emit('showBanner', false)
+      }
+    }
+  },
+  computed: {
+    oliveAppLink: function () {
+      if (this.mobileDevice === 'ios') {
+        return 'https://apps.apple.com/kr/app/%EC%98%AC%EB%A6%AC%EB%B8%8C%EC%98%81/id873779010'
+      } else if (this.mobileDevice === 'android') {
+        return 'https://play.google.com/store/apps/details?id=com.oliveyoung&hl=ko&gl=US'
+      }
+    }
+  },
+  components: {
+    IconClose
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header-banner {
+  display: flex;
+  background-color: #676b7e;
+}
+.header-banner__link {
+  flex: 1 1 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 14px;
+  word-break: keep-all;
+  word-wrap: break-word;
+  &:before {
+    display: block;
+    width: 48px;
+    height: 48px;
+    margin-right: 6px;
+    background: url('~@/assets/images/common/ico_app.png') center center / 32px no-repeat;
+    content: "";
+  }
+}
+.header-banner__msg {
+  display: block;
+  font-weight: 700;
+  font-size: 14px;
+  color: #fff;
+  letter-spacing: -0.5px;
+  line-height: 20px;
+  &--highlight {
+    display: inline;
+    color: #ffff00;
+  }
+}
+.header-banner__btn {
+  flex: 0 0 auto;
+  width: 48px;
+  background-color: #5e6174;
+  cursor: pointer;
+}
+</style>
