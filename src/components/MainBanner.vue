c829051d5f94a9438c30f8c43293eb910883588a
MostOneBrush <w_hyacinth.choi@kakao.com>
2021-07-15 20:22:49 +0900

    메인 배너 관련 작업 업로드

--- src/components/MainBanner.vue
+++ src/components/MainBanner.vue
@@ -1,21 +1,169 @@
 <template>
-  <swiper ref="mainBannerSwiper" :options="swiperOption">
-    <swiper-slide>
-    </swiper-slide>
-  </swiper>
+  <article class="main-banner__article">
+    <swiper :options="swiperOption" :key="swiperOption.loop" v-if="isLoading">
+      <swiper-slide
+        v-for="(item, index) in bannerList"
+        :key="index">
+        <MainBannerItem
+          :bannerLink="bannerList[index].bannerUrl"
+          :bannerImage="`${require(`@/assets/images/banner/${bannerList[index].bannerImage}`)}`"
+          :bannerDesc="bannerList[index].bannerDesc"
+          :bannerDescColor="bannerList[index].bannerDescColor"
+          :aria-label="bannerList[index].bannerName" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+    <button type="button" class="main-banner__btn" aria-label="메인배너 전체보기" @click="isMainBannerModalClose = 'false'">
+      <IconPlus size="20" stroke="1" color="#fff" />
+    </button>
+    <PopupLayout
+      class="main-banner__modal"
+      :modalId="'modalMainBanner'"
+      :modalName="mainBannerModalName"
+      :isModalClose="isMainBannerModalClose"
+      @modalClose="isModalClose">
+      <MainBannerItem
+        v-for="(item, index) in bannerList"
+        :key="index"
+        :bannerLink="bannerList[index].bannerUrl"
+        :bannerImage="`${require(`@/assets/images/banner/${bannerList[index].bannerImage}`)}`"
+        :bannerDesc="bannerList[index].bannerDesc"
+        :bannerDescColor="bannerList[index].bannerDescColor"
+        :aria-label="bannerList[index].bannerName" />
+    </PopupLayout>
+  </article>
 </template>
 
 <script>
+import axios from 'axios'
 import {swiper, swiperSlide} from 'vue-awesome-swiper'
+import MainBannerItem from '@/components/MainBannerItem'
+import PopupLayout from '@/components/layout/PopupLayout'
+import IconPlus from '@/components/icon/IconPlus'
+
 export default {
   name: 'MainBanner',
+  data () {
+    return {
+      bannerList: [],
+      isMainBannerModalClose: 'true',
+      swiperOption: {
+        slidesPerView: 1,
+        loop: true,
+        autoplay: {
+          delay: 3500,
+          disableOnInteraction: false
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction'
+        },
+        on: {
+        }
+      }
+    }
+  },
+  methods: {
+    fetchBannerData: async function () {
+      try {
+        const response = await axios.get('/static/js/mainBanner.json')
+        const responseStatus = response.data.length > 0
+        this.$emit('mainBannerLoading', responseStatus)
+
+        this.bannerList = response.data
+        this.isLoading = true
+      } catch (error) {
+      }
+    },
+    isModalClose: function (val) {
+      this.isMainBannerModalClose = val
+    }
+  },
+  computed: {
+    isShowBanner: function () {
+      return this.bannerList.length > 0 && this.isLoading
+    },
+    mainBannerModalName: function () {
+      return `전체보기 (${this.bannerList.length})`
+    }
+  },
+  created () {
+    this.fetchBannerData()
+  },
+  mounted () {
+  },
   components: {
     swiper,
-    swiperSlide
+    swiperSlide,
+    MainBannerItem,
+    PopupLayout,
+    IconPlus
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.swiper-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.swiper-slide {
+  /deep/ .main-banner__title {
+    margin-left: -4%;
+  }
+  /deep/ .main-banner__sub-txt {
+    margin-left: -6%;
+  }
+  &-active {
+    /deep/ .main-banner__title {
+      margin-left: 0;
+      transition: margin 0.8s;
+    }
+    /deep/ .main-banner__sub-txt {
+      margin-left: 0;
+      transition: margin 1.5s;
+    }
+  }
+}
+.swiper-pagination {
+  position: absolute;
+  left: auto;
+  right: calc(6.5% + 32px);
+  bottom: 5.26315%;
+  width: auto;
+  padding: 3px 10px;
+  font-size: 0.8em;
+  color: #fff;
+  line-height: 20px;
+  border-radius: 40px;
+  vertical-align: top;
+  background-color: rgba(0,0,0,0.5);
+}
+.main-banner__article {
+  position: relative;
+  padding-bottom: 76%;
+}
+.main-banner__btn {
+  position: absolute;
+  right: 6.5%;
+  bottom: 5.26315%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 26px;
+  height: 26px;
+  border-radius: 50%;
+  background-color: rgba(0,0,0,0.5);
+  z-index: 1;
+}
+.main-banner__modal {
+  .main-banner__link {
+    &:nth-child(n+2) {
+      margin-top: 16px;
+    }
+  }
+}
 </style>
---
+++ src/components/MainBannerItem.vue
@@ -0,0 +1,83 @@
+<template>
+  <a
+    :href="bannerLink"
+    class="main-banner__link">
+      <span
+        class="main-banner__image"
+        :style="{ backgroundImage: `url(${bannerImage})`}"></span>
+        <div class="main-banner__txt" v-if="bannerDesc.length > 0">
+          <strong class="main-banner__title">
+            <span class="main-banner__title--block" v-html="bannerDesc[0]" :style="{color: bannerDescColor[0]}"></span>
+            <span class="main-banner__title--block" v-html="bannerDesc[1]" :style="{color: bannerDescColor[1]}"></span>
+          </strong>
+          <p class="main-banner__sub-txt" v-if="bannerDesc[2]" :style="{color: bannerDescColor[2]}">{{bannerDesc[2]}}</p>
+      </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'MainBannerItem',
+  props: {
+    bannerLink: {
+      type: String,
+      default: ''
+    },
+    bannerImage: {
+      type: String,
+      default: ''
+    },
+    bannerDesc: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    bannerDescColor: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main-banner__link {
+  display: block;
+  flex-direction: column;
+  position: relative;
+  font-size: 100vw;
+}
+.main-banner__image {
+  display: block;
+  width: 100%;
+  padding-bottom: 76%;
+  background: center center / contain no-repeat;
+}
+.main-banner__txt {
+  position: absolute;
+  top: 10.5263%;
+  left: 6.5%;
+  width: 87%;
+  z-index: 1;
+}
+.main-banner__title {
+  display: block;
+  font-weight: 700;
+  font-size: 0.065em;
+  color: #333;
+  text-align: left;
+  line-height: 1.4;
+  &--block {
+    display: block;
+  }
+}
+.main-banner__sub-txt {
+  display: block;
+  margin-top: 2%;
+  font-size: 0.035em;
+  color: #333;
+}
+</style>
