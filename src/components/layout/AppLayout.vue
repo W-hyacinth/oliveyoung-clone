a11312f9a151b1a5a9e1da0aa3d0a631cc8a58f9
MostOneBrush <w_hyacinth.choi@kakao.com>
2021-07-15 20:21:39 +0900

    기본 레이아웃 2종 추가

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <section class="header__section">
+      <HeaderBanner v-if="isHeaderBannerDisplay" @showBanner="isHeaderBannerShow" />
+      <Header />
+      <Menu />
+    </section>
+    <main id="main" class="main">
+      <slot name="default"></slot>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import Menu from '@/components/Menu'
+import HeaderBanner from '@/components/HeaderBanner'
+export default {
+  name: 'AppLayout',
+  data () {
+    return {
+      isHeaderBannerDisplay: true
+    }
+  },
+  methods: {
+    isHeaderBannerShow: function (val) {
+      this.isHeaderBannerDisplay = val
+    }
+  },
+  created () {
+    this.isHeaderBannerDisplay = this.getCookie('headerBanner') !== 'false'
+  },
+  components: {
+    Header,
+    Footer,
+    Menu,
+    HeaderBanner
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+#main {
+  flex: 1 1 auto;
+}
+</style>
---
+++ src/components/layout/PopupLayout.vue
@@ -0,0 +1,97 @@
+<template>
+  <section
+    class="modal__section"
+    role="modal"
+    aria-modal="true"
+    :aria-labelledby="modalId"
+    :aria-hidden="isModalClose">
+    <div class="modal-header">
+      <h1 :id="modalId" class="modal-header__title">{{ modalName }}</h1>
+    </div>
+    <article class="modal__article">
+      <slot name="default"></slot>
+    </article>
+    <button type="button" class="modal__btn modal__btn--close" :aria-label="`${modalName} 모달 팝업 닫기`" @click="modalClose">
+      <IconClose />
+    </button>
+  </section>
+</template>
+
+<script>
+import IconClose from '@/components/icon/IconClose'
+export default {
+  name: 'PopupLayout',
+  data () {
+    return {
+    }
+  },
+  props: {
+    modalId: {
+      type: String,
+      default: function () {
+        const randomStr = Math.random().toString(36).substr(2, 6)
+        return `modal__id--${randomStr}`
+      }
+    },
+    modalName: {
+      type: String,
+      default: '팝업제목'
+    },
+    isModalClose: {
+      type: String,
+      default: 'true',
+      validator: function (value) {
+        return ['true', 'false'].indexOf(value) !== -1
+      }
+    }
+  },
+  methods: {
+    modalClose: function () {
+      this.$emit('modalClose', 'true')
+    }
+  },
+  components: {
+    IconClose
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.modal__section {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  flex-direction: column;
+  background-color: #fff;
+  z-index: 10;
+  &[aria-hidden="false"]{
+    display: flex;
+  }
+}
+.modal-header {
+  flex: 0 0 auto;
+  padding: 15px 16px;
+  border-bottom: 1px solid #ddd;
+}
+.modal-header__title {
+  padding: 0 20px;
+  font-weight: 700;
+  font-size: 20px;
+  color: #000;
+  text-align: center;
+  line-height: 24px;
+}
+.modal__article {
+  overflow-x: hidden;
+  overflow-y: auto;
+  flex: 1 1 auto;
+  padding: 20px 16px calc(20px + env(safe-area-inset-bottom));
+}
+.modal__btn--close {
+  position: absolute;
+  top: 15px;
+  right: 16px;
+}
+</style>
