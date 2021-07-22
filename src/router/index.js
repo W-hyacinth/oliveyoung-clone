fd717d5c4928ef1c069ae86fb374657af6658d16
MostOneBrush <w_hyacinth.choi@kakao.com>
2021-07-22 10:49:54 +0900

    netlify 파일 내 리다이렉트 설정 후 테스트 배포

--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,12 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    // {
-    //   path: '/',
-    //   redirect: '/main'
-    // },
     {
       path: '/',
+      redirect: '/main'
+    },
+    {
+      path: '/main',
       name: 'Main',
       component: () => import('@/views/Main')
     },
